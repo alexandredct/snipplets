046d8ec0556f6dbffc825217c1a3a031e7af5d07
Alexandre Trindade <alexandredct@gmail.com>
2019-02-04 20:24:14 -0200

    Create oracle-listar_grants_usuario.sql
    Listar grants de um usuário

---
+++ sql/oracle-listar_grants_usuario.sql
@@ -0,0 +1,19 @@
+--Listar todos os grants para um usuário
+
+select
+  'grant '|| privilegios || ' on ' || owner || '.' || table_name || ' to ' || grantee || ';' as sql
+from (
+  select
+    LISTAGG(privilege, ', ') WITHIN GROUP (ORDER BY privilege asc) as privilegios,
+    owner, 
+    table_name, 
+    grantee
+  FROM DBA_TAB_PRIVS 
+  WHERE 
+    GRANTEE = 'USER'
+  group by 
+    owner,
+    table_name, 
+    grantee
+)
+;
